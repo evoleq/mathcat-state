2dcd9f007677bbb3a6c88492d1c60dec69a3955b
Florian Schmidt <florian.schmidt@platis.de>
2020-06-18 17:25:12 +0200

    Applicative structure

--- src/commonMain/kotlin/org/evoleq/math/cat/suspend/monad/state/scoped-suspended-state.kt
+++ src/commonMain/kotlin/org/evoleq/math/cat/suspend/monad/state/scoped-suspended-state.kt
@@ -16,42 +16,119 @@
 package org.evoleq.math.cat.suspend.monad.state
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.coroutineScope
 import org.evoleq.math.cat.marker.MathCatDsl
 import org.evoleq.math.cat.suspend.morphism.ScopedSuspended
 import org.evoleq.math.cat.suspend.morphism.by
 
+/**
+ * Scoped suspended state
+ */
+interface ScopedSuspendedState<S, T> : ScopedSuspended<S, Pair<T, S>>
 
-interface ScopedSuspendedState<S, T> : ScopedSuspended<S, Pair<T, S>> {
-    suspend infix fun <T1> map(f: suspend CoroutineScope.(T) -> T1): ScopedSuspendedState<S, T1> = ScopedSuspendedState {
-            s -> with(by(this@ScopedSuspendedState)(s)){
-            Pair(f(first),second)
-        }
-    }
-    
-    suspend fun <U> bind(arrow: suspend CoroutineScope.(T)->ScopedSuspendedState<S, U>): ScopedSuspendedState<S, U> = (this map arrow).multiply()
-}
-@Suppress("FunctionName")
+/**
+ * Constructor function for the [ScopedSuspendedState]
+ */
 @MathCatDsl
+@Suppress("FunctionName")
 fun <S, T> ScopedSuspendedState(state: suspend CoroutineScope.(S)->Pair<T, S>): ScopedSuspendedState<S, T> = object : ScopedSuspendedState<S, T> {
     override val morphism: suspend CoroutineScope.(S) -> Pair<T, S>
         get() = state
 
 }
+/**********************************************************************************************************************
+ *
+ * Functorial structure
+ *
+ **********************************************************************************************************************/
+@MathCatDsl
+suspend infix fun <S, T, T1> ScopedSuspendedState<S, T>.map(f: suspend CoroutineScope.(T) -> T1): ScopedSuspendedState<S, T1> = ScopedSuspendedState {
+    s -> with(by(this@map)(s)){
+        Pair(f(first), second)
+    }
+}
+
+/**********************************************************************************************************************
+ *
+ * Applicative structure
+ *
+ **********************************************************************************************************************/
+
+/**
+ * Apply function of the applicative [ScopedSuspendedState]
+ */
+@MathCatDsl
+suspend fun <R, S, T> ScopedSuspendedState<R, suspend CoroutineScope.(S)->T>.apply()
+    : suspend CoroutineScope.(ScopedSuspendedState<R, S>)->ScopedSuspendedState<R, T> = {
+    stateS -> ScopedSuspendedState{ r: R ->
+        val  pR = by(stateS)(r)
+        val pF = by(this@apply)(pR.second)
+        with(pF.first) {
+            Pair(this(pR.first), pF.second)
+        }
+    }
+}
+
+/**
+ * Apply function of the applicative [ScopedSuspendedState]
+ */
+@MathCatDsl
+suspend infix fun <R, S, T> ScopedSuspendedState<R, suspend CoroutineScope.(S)->T>.apply(
+    next: ScopedSuspendedState<R, S>
+): ScopedSuspendedState<R, T> =
+    coroutineScope { this@apply.apply()(next) }
+
+
+/**********************************************************************************************************************
+ *
+ * Monadic structure
+ *
+ **********************************************************************************************************************/
+/**
+ * Return function of the [ScopedSuspendedState] monad
+ */
 @Suppress("FunctionName")
 @MathCatDsl
 fun <S, T> ReturnState(t: T): ScopedSuspendedState<S, T> = ScopedSuspendedState {
     s -> Pair(t,s)
 }
 
+/**
+ * Multiplication of the [ScopedSuspendedState] monad
+ */
 @MathCatDsl
-fun <S, T> ScopedSuspendedState<S, ScopedSuspendedState<S, T>>.multiply() : ScopedSuspendedState<S, T> = ScopedSuspendedState {
-        s -> with(by(this@multiply)(s)){
-    by(first)(second)
-}
+fun <S, T> ScopedSuspendedState<S, ScopedSuspendedState<S, T>>.multiply() : ScopedSuspendedState<S, T> =
+    ScopedSuspendedState { s -> with(by(this@multiply)(s)){
+        by(first)(second)
+    }
 }
 
-interface KlScopedSuspendedState<B, S, T> : ScopedSuspended<S, ScopedSuspendedState<B, T>>
+/**
+ * Bind function of the [ScopedSuspendedState] monad
+ */
+@MathCatDsl
+suspend fun <S, T, U> ScopedSuspendedState<S, T>.bind(
+    arrow: suspend CoroutineScope.(T)->ScopedSuspendedState<S, U>
+): ScopedSuspendedState<S, U> = (this map arrow).multiply()
 
+/**
+ * Kleisli [ScopedSuspendedState]
+ */
+interface KlScopedSuspendedState<B, S, T> : ScopedSuspended<S, ScopedSuspendedState<B, T>> {
+    /**
+     * [KlScopedSuspendedState] is functorial, i.e. for fixed S, I the map T -> KlScopedSuspendedState<S, I T> is a functor.
+     * This is true because this map is just a composition Hom_i ° State<S, _> of the hom- and the state-functor
+     */
+    @MathCatDsl
+    suspend infix fun <S, I, O1, O2> KlScopedSuspendedState<S, I, O1>.map(f: suspend CoroutineScope.(O1)->O2): KlScopedSuspendedState<S, I , O2> =
+        KlScopedSuspendedState {
+            input -> by(this@map)(input) map f
+        }
+}
+
+/**
+ * Constructor function for [KlScopedSuspendedState]
+ */
 @MathCatDsl
 @Suppress("FunctionName")
 fun<B, S, T>  KlScopedSuspendedState(arrow: suspend CoroutineScope.(S)-> ScopedSuspendedState<B, T>): KlScopedSuspendedState<B, S, T> = object : KlScopedSuspendedState<B, S, T> {
@@ -59,12 +136,18 @@ fun<B, S, T>  KlScopedSuspendedState(arrow: suspend CoroutineScope.(S)-> ScopedS
         get() = arrow
 }
 
+/**
+ * Identity element of the [ScopedSuspendedState] monad
+ */
 @Suppress("FunctionName")
 @MathCatDsl
 fun <B, S> KlReturnState(): KlScopedSuspendedState<B, S, S> = KlScopedSuspendedState {
     s -> ReturnState(s)
 }
 
+/**
+ * Multiplication of the [ScopedSuspendedState] monad
+ */
 suspend operator fun <B, R, S, T> KlScopedSuspendedState<B, R, S>.times(other: KlScopedSuspendedState<B, S, T>): KlScopedSuspendedState<B, R, T> =
     KlScopedSuspendedState {
             r -> by(this@times)(r).map(by(other)).multiply()
